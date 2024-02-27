fb99b5be60c1b2c4755d9fce05ffcd772686347e
Natalie Mann <NatalieMannOfficial@gmail.com>
2024-02-27 07:38:05 -0500

    Add package

--- APCompSci/Employee/Employee.java
+++ APCompSci/Employee/Employee.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package APCompSci.Employee;
 
-/**
- *
- * @author mysti
- */
-
-
- public class Employee {
+public class Employee {
     //static variable to keep track of total employees and salaries
 
     public static double totSal = 0;
